7e97b15ffcd3eacddb63da009b2d615dfbedeef5
ligiphilip <ligiphilip92@gmail.com>
2021-11-09 22:13:19 +0530

    Multiple file upload

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -17,18 +17,42 @@ class PostController extends Controller
 
         $current_user = get_user();
 
-        $post_data['post_type_id'] = $request->post_type_id;
-        $post_data['title'] = $request->title;
-        $post_data['content'] = $request->content;
-        $post_data['image'] = $request->image;
-        $post_data['video'] = $request->video;
-        $post_data['audio'] = $request->audio;
-        $post_data['live_stream'] = $request->live_stream;
         $post_data['when_to_post_id'] = $request->when_to_post_id;
         $post_data['who_can_see_post_id'] = $request->who_can_see_post_id;
         $post_data['user_id'] = $current_user->id;
-        
-        $post = Post::create($post_data);
+
+        switch ($request->post_type_id) {
+        case 1:
+            $post_data['title'] = $request->title;
+            $post_data['content'] = $request->content;
+            $post = Post::create($post_data);
+            break;
+
+        case 2:
+            $post_data['image'] = $request->image;
+            $post_data['content'] = $request->content;
+            $post = Post::create($post_data);
+            break;
+
+        case 3:
+            $post_data['video'] = $request->video;
+            $post_data['content'] = $request->content;
+            $post = Post::create($post_data);
+            break;
+            
+        case 4:
+            $post_data['audio'] = $request->audio;
+            $post_data['content'] = $request->content;
+            $post = Post::create($post_data);
+            break;
+            
+        case 5:
+            $post_data['live_stream'] = $request->live_stream;
+            $post_data['content'] = $request->content;
+            $post = Post::create($post_data);
+            break;
+        }
+      
         if (!empty($post)) {
             $data = $post;
             $status_code = SUCCESSCODE;
